6090cd6a19bd425d531ca712119cfad75dbc2587
ме <polinabliznyk@gmail.com>
2025-05-12 11:22:30 +0300

    finish item page

--- item/item.js
+++ item/item.js
@@ -1,5 +1,20 @@
 document.addEventListener('DOMContentLoaded', async function() {
-   const itemId = localStorage.getItem('selectedItemId');
+    const themeToggle = document.getElementById('color-button');
+    const html = document.documentElement;
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        html.setAttribute('data-theme', savedTheme);
+    }
+
+    themeToggle.addEventListener('click', function() {
+        const currentTheme = html.getAttribute('data-theme');
+        const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+        
+        html.setAttribute('data-theme', newTheme);
+        localStorage.setItem('theme', newTheme);
+    });
+   
+    const itemId = localStorage.getItem('selectedItemId');
    
     try {
         const response = await fetch('../data.json');
